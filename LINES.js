32b59262ab233f010c2dc5c7bf99f43b001db9ec
fdoflorenzano <faflorenzano@uc.cl>
2016-04-13 09:29:20 -0300

    rotate function, continous transition, Radial form with different angles

--- LINES.js
+++ LINES.js
@@ -3,25 +3,36 @@ function RADIAL(N, radius, cx, cy, rotation = 0, M = 10) {
   	for (var i = 0; i < N; i++) {
       lines.push({'x1': cx + radius  * Math.cos(Math.PI * 2 / N * (i - 1) % N),
                  'y1': cy + radius  * Math.sin(Math.PI * 2 / N * (i - 1) % N),
-                 'x2': cx + radius * Math.cos(Math.PI * 2 * i / N) - radius  * Math.cos(Math.PI * 2 * i / N),
-                 'y2': cy + radius * Math.sin(Math.PI * 2 * i / N) - radius  * Math.sin(Math.PI * 2 * i / N)});
+                 'x2': cx,
+                 'y2': cy});
     }
     var web = [];
     for( i = 0; i < N; i++){
       web = web.concat(WEB(lines[i], lines[(i+1)%N], M));
     }
     lines = lines.concat(web);
-    lines.forEach(function(line){
-      var x1 = (line.x1-cx)*Math.cos(Math.PI*rotation/180) - (line.y1-cy)*Math.sin(Math.PI*rotation/180);
-      var y1 = (line.x1-cx)*Math.sin(Math.PI*rotation/180) + (line.y1-cy)*Math.cos(Math.PI*rotation/180);
-      var x2 = (line.x2-cx)*Math.cos(Math.PI*rotation/180) - (line.y2-cy)*Math.sin(Math.PI*rotation/180);
-      var y2 = (line.x2-cx)*Math.sin(Math.PI*rotation/180) + (line.y2-cy)*Math.cos(Math.PI*rotation/180);
-      line.x1 = cx + x1;
-      line.x2 = cx + x2;
-      line.y1 = cy + y1;
-      line.y2 = cy + y2;
-    });
+    lines = ROTATE(lines, rotation, cx, cy);
   	return lines;
+}
+
+function RADIAL_ANGLE(angles, radius, cx, cy, rotation = 0, M = 10){
+  var lines = [];
+  if (angles.reduce((a, b) => a + b, 0) != 360) { return lines;}
+  var sum = 0;
+  for (var i = 0; i < angles.length; i++) {
+    sum += angles[i];
+    lines.push({'x1': cx + radius  * Math.cos(Math.PI * sum / 180),
+               'y1': cy + radius  * Math.sin(Math.PI *  sum / 180),
+               'x2': cx,
+               'y2': cy});
+  }
+  var web = [];
+  for( i = 0; i < angles.length; i++){
+    web = web.concat(WEB(lines[i], lines[(i+1)%(angles.length)], M));
+  }
+  lines = lines.concat(web);
+  lines = ROTATE(lines, rotation, cx, cy);
+  return lines;
 }
 
 function WEB(line1, line2, M){
@@ -34,5 +45,19 @@ function WEB(line1, line2, M){
       'y2': line2.y2 + i*(line2.y1 - line2.y2)/M
     });
   }
+  return lines;
+}
+
+function ROTATE(lines, rotation, cx, cy){
+  lines.forEach(function(line){
+    var x1 = (line.x1-cx)*Math.cos(Math.PI*rotation/180) - (line.y1-cy)*Math.sin(Math.PI*rotation/180);
+    var y1 = (line.x1-cx)*Math.sin(Math.PI*rotation/180) + (line.y1-cy)*Math.cos(Math.PI*rotation/180);
+    var x2 = (line.x2-cx)*Math.cos(Math.PI*rotation/180) - (line.y2-cy)*Math.sin(Math.PI*rotation/180);
+    var y2 = (line.x2-cx)*Math.sin(Math.PI*rotation/180) + (line.y2-cy)*Math.cos(Math.PI*rotation/180);
+    line.x1 = cx + x1;
+    line.x2 = cx + x2;
+    line.y1 = cy + y1;
+    line.y2 = cy + y2;
+  });
   return lines;
 }
